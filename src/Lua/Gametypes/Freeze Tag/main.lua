8e52504e3ff6955da489ee7c4a87392ff717b069
Jisk <danielthesmartypants2017@gmail.com>
2025-07-09 04:31:54 -0500

    disable_item_mapthing setting (not functional)

--- src/Lua/Gametypes/Freeze Tag/main.lua
+++ src/Lua/Gametypes/Freeze Tag/main.lua
@@ -2,4 +2,5 @@ MM.RegisterGametype("Freeze Tag", {
 	max_time = 4*60*TICRATE;
 	disable_overtime = true;
 	disable_sheriff = true;
+	disable_item_mapthing = true; -- includes interactions that drop items.
 })
